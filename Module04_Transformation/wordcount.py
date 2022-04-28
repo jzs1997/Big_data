c80239b32f63db41a55f01083e997172e5d23ee1
YamataHermione <shijuzheng1997@126.com>
2022-04-28 16:17:00 -0700

    more

---
+++ Module04_Transformation/wordcount.py
@@ -0,0 +1,13 @@
+import sys
+
+from pyspark.sql import SparkSession
+# Change permissions for file and folders
+# https://www.educative.io/edpresso/how-to-resolve-the-permission-denied-error-in-linux
+
+#print sys info
+
+#create a spark session
+
+#do word count job
+
+#stop session
