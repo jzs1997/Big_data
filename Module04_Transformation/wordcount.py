27e2b11e69197f453cddb4fa9db1e9a466b37d5a
YamataHermione <shijuzheng1997@126.com>
2022-04-28 17:48:33 -0700

    todo

--- Module04_Transformation/wordcount.py
+++ Module04_Transformation/wordcount.py
@@ -3,10 +3,13 @@ import sys
 from pyspark.sql import SparkSession
 # Change permissions for file and folders
 # https://www.educative.io/edpresso/how-to-resolve-the-permission-denied-error-in-linux
-
+input_path = 'input.txt'
 #print sys info
-
+print("this is the name of the script:", sys.argv[0])
+print("number of arguments: ", len(sys.argv))
+print("The arguments are:", str(sys.argv))
 #create a spark session
+
 
 #do word count job
 
